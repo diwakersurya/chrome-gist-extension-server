d7641bb1cef59b7ce4411d30e94114408b785f7b
diwaker_surya <diwaker.singh@games24x7.com>
2016-07-13 13:56:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var errorhandler = require("errorhandler");
+var request=require("superagent");
+var githubURL="https://github.com/login/oauth/access_token";
+var app = express();
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function ( req,res) {
+	var codeToSend=req.query.code;//temporary code provided by github to get the access token
+	request.post(githubURL)
+		  .send({ client_id: process.env.CLIENT_ID, client_secret: process.env.CLIENT_SECRET, code: codeToSend })
+		  .set('Accept', 'application/json')
+		  .end(function(errInternal, resInternal){
+		    if (errInternal) {console.log(errInternal); return }
+		    //here is the acces token
+			return res.json({access_token:resInternal.body.access_token});
+		  });
+});
+
+app.get("/check", function ( req,res) {
+	return res.json({result:"wroking properly"});
+});
+app.use(errorhandler());
+
+
+var server = require("http").createServer(app).listen(4567,function() {
+	console.log("Server listening now.... ");
+});
+ 
